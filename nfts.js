a9495536f44c309a38176ff3ea6b73ac748d780f
Jabril Abdilahi <66794425+JabrilAbdilahi@users.noreply.github.com>
2021-12-05 17:27:07 -0600

    Add files via upload

---
+++ nfts.js
@@ -0,0 +1,11 @@
+let js="get your nfts abo 0.0002eth is 82 dollars get it steph curry has on thats 218k jimmy falon etc"
+
+let firstName ="jabril";
+
+console.log(firstName);
+console.log(firstName); 
+console.log(firstName);
+
+let thefamily="jabril,jazmine,mustafa,makial,noor, hamdi";
+
+console.log(thefamily);
